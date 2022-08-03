e474583b47d397b1082bb624e25daf8d31537ef1
DreamingJohnny <johnnydreaming@gmail.com>
2022-08-03 17:45:51 +0200

    Added icon to cargo
    I added a sprite renderer that is activated and shows an sprite of the content above the crates, I also began building a bool that decided when the icon should be showed.

--- Skippers Scraptastic Adventure/Assets/00_Content/Scripts/Cargo.cs
+++ Skippers Scraptastic Adventure/Assets/00_Content/Scripts/Cargo.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Cargo : MonoBehaviour {
+
+	[SerializeField] private GameObject contentIcon;
 
 	[SerializeField] private Material standardMat;
 	[SerializeField] private Material selectableMat;
@@ -14,6 +18,8 @@ public class Cargo : MonoBehaviour {
 	[SerializeField] private float ballistic;
 	[SerializeField] private float value;
 
+	public bool isRevealed;
+
 	public bool IsSelected;
 	public float GetFlammability { get { return flammability; } }
 	public float GetBallistic { get { return ballistic; } }
@@ -21,18 +27,20 @@ public class Cargo : MonoBehaviour {
 
 	private void Start() {
 		GetComponent<MeshRenderer>().material = standardMat;
+		contentIcon.SetActive(true);
 	}
 
 	private void OnMouseEnter() {
 		GetComponent<MeshRenderer>().material = selectableMat;
+		if(isRevealed) { contentIcon.SetActive(true); }
 	}
 
 	private void OnMouseExit() {
 		GetComponent<MeshRenderer>().material = standardMat;
+		contentIcon.SetActive(false);
 	}
 
 	private void OnDestroy() {
 		Debug.Log("This object will now be destroyed...");
 	}
-
 }
